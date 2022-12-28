3dc0d5a43f73d9992f015347e9416ab94ad20d79
Viniflecha1 <99683761+Viniflecha1@users.noreply.github.com>
2022-12-28 17:43:25 -0400

    Inserção do código do firebase

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCTqalagJQ2JuoelEFMfDYELoz0RuQjb7E",
+  authDomain: "aulateste-29020.firebaseapp.com",
+  projectId: "aulateste-29020",
+  storageBucket: "aulateste-29020.appspot.com",
+  messagingSenderId: "121026569256",
+  appId: "1:121026569256:web:a86cba956e9356557b811d"
+};
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
